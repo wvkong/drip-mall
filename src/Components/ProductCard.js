ac534baedd5c6ab5cc6c3ac32d39b948434fb0f9
+ <+>
2022-10-07 22:53:32 +0300

    Edited product card

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -6,7 +6,7 @@ function ProductCard({product, cartIt}) {
       <div class="product-image">
         <span class="discount-tag">{product.discount}% off</span>
         <img src={product.image} class="product-thumb" alt="" />
-        <button class="card-btn" onClick={() => cartIt(product)}>add to card</button>
+        <button class="card-btn" onClick={() => cartIt(product)}>add to cart</button>
       </div>
       <div class="product-info">
         <h2 class="product-brand">{product.title}</h2>
